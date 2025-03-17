df088c8994b574e7850575ea48e7cf7a491969c8
Shafi Akinropo <sakinropo@gmail.com>
2025-03-17 12:34:09 +0100

    update

---
+++ app/store/layout.tsx
@@ -0,0 +1,10 @@
+// app/store/layout.tsx
+// import { Suspense } from "react";
+
+export default function StoreLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
